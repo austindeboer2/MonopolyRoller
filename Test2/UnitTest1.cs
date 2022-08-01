06cbd1ac6d7ce906a482a9dd2eac6fa95f3831ca
yellowlimo17 <55175178+yellowlimo17@users.noreply.github.com>
2022-08-01 12:33:52 -0400

    fixed

---
+++ Test2/UnitTest1.cs
@@ -0,0 +1,36 @@
+namespace Test2
+{
+    public class Tests
+    {
+        Monopoly m;
+        Random r;
+        [SetUp]
+        public void Setup()
+        {
+            m = new Monopoly();
+            r = new Random();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            bool passed = true;
+            for (int i = 0; i < 1000; i++)
+            {
+                int a = r.Next(1, 7);
+                int b = r.Next(1, 7);
+                int total = a + b;
+                if (a == b)
+                {
+                    if (!m.Roll(2, a, b).Equals("You move " + total + ". Roll again.")) passed = false;
+                }
+                else
+                {
+                    if (!m.Roll(2, a, b).Equals("You move " + total + ".")) passed = false;
+                }
+            }
+            if (passed) Assert.Pass();
+            else Assert.Fail("An output in your 2nd roll was found to be incorrect.");
+        }
+    }
+}
